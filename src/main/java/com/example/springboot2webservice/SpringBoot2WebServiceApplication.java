18375a7ab7a1462fcb11412ac8730fc205595906
alalshow <alalshow@hanmail.net>
2021-06-02 14:56:08 +0900

    Initial commit

---
+++ src/main/java/com/example/springboot2webservice/SpringBoot2WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2WebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2WebServiceApplication.class, args);
+    }
+
+}
