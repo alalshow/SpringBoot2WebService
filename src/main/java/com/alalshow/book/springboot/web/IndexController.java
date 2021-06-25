2c0e7cce09f372e44d8679ce63c73f5fdf8edafc
alalshow <alalshow@hanmail.net>
2021-06-25 22:37:12 +0900

    Chapter04 - 게시판 CRUD 완성

--- src/main/java/com/alalshow/book/springboot/web/IndexController.java
+++ src/main/java/com/alalshow/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.alalshow.book.springboot.web;
 
+import com.alalshow.book.springboot.config.auth.LoginUser;
+import com.alalshow.book.springboot.config.auth.dto.SessionUser;
 import com.alalshow.book.springboot.service.posts.PostsService;
 import com.alalshow.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -30,6 +39,7 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
+
         return "posts-update";
     }
 }
